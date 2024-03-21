82b5ac9f521215830e115646b9976ad189b216fc
MatteoRuffa <cortese_17@protonmail.com>
2024-03-21 14:26:08 +0100

    add MILESTONE 0

--- js-script/script.js
+++ js-script/script.js
@@ -17,3 +17,45 @@ BONUS 3:
 Permettere l'aggiunta di un nuovo membro del team
 */
 
+let team = [];
+
+let member = {
+    nome: '',
+    ruolo: '',
+    foto: ''
+}
+
+// MILESTONE 0:
+team = [
+    {
+        nome: 'Wayne Barnett',
+        ruolo: 'Founder & CEO',
+        foto: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        nome: 'Angela Caroll',
+        ruolo: 'Chief Editor',
+        foto: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        nome: 'Walter Gordon',
+        ruolo: 'Office Manager',
+        foto: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        nome: 'Angela Lopez',
+        ruolo: 'Social Media Manager',
+        foto: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        nome: 'Scott Estrada',
+        ruolo: ' Developer',
+        foto: 'scott-estrada-developer.jpg'
+    },
+    {
+        nome: 'Barbara Ramos',
+        ruolo: 'Graphic Designer',
+        foto: 'barbara-ramos-graphic-designer.jpg'
+    }
+]
+console.log(team);
